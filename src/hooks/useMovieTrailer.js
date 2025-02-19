3a81210efdcde09a1c89933ef630e13d32c9cdb8
jorjishasan <dev.jorjis@gmail.com>
2025-02-20 05:50:44 +0600

    fix: handle movieId validation and ensure numeric ID is used for trailer fetching

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -8,9 +8,14 @@ const useMovieTrailer = (movieId) => {
   const trailerMap = useSelector((store) => store.movies.trailerMap);
 
   const getMovieVideos = async () => {
+    if (!movieId) return;
+    
+    // Ensure movieId is a number
+    const numericId = typeof movieId === 'string' ? movieId.split('-')[0] : movieId;
+    
     try {
       const data = await fetch(
-        `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+        `https://api.themoviedb.org/3/movie/${numericId}/videos?language=en-US`,
         TMDB_API_OPTIONS,
       );
       const json = await data.json();
@@ -19,7 +24,7 @@ const useMovieTrailer = (movieId) => {
         (video) => video.type === "Trailer",
       );
       const trailer = filterData.length ? filterData[0] : json.results[0];
-      dispatch(addTrailer({ movieId, trailer }));
+      dispatch(addTrailer({ movieId: numericId, trailer }));
     } catch (error) {
       console.error("Error fetching movie trailer:", error);
     }
@@ -29,7 +34,9 @@ const useMovieTrailer = (movieId) => {
     if (!trailerMap[movieId]) {
       getMovieVideos();
     }
-  }, []);
+  }, [movieId, trailerMap]);
+
+  return trailerMap[movieId];
 };
 
 export default useMovieTrailer;
