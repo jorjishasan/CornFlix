ef59775fecd4527f9c9a1f9ee43591caee881b34
jorjishasan <dev.jorjis@gmail.com>
2025-02-19 17:20:02 +0600

    feat: add credit reducer to Redux store for credit management

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesReducer from "./moviesSlice";
-import aiSearchReducer from "./aiSearchSlice";
 import langReducer from "./langSlice";
 import clickedMovieReducer from "./clickedMovieSlice";
+import creditReducer from "./creditSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     movies: moviesReducer,
-    aiSearch: aiSearchReducer,
     lang: langReducer,
     clickedMovie: clickedMovieReducer,
+    credits: creditReducer,
   },
 });
 
